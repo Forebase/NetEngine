e422fae81f89a898e93180e8398cd7892153265f
aidankhogg <aidankhogg@gmail.com>
2026-06-30 16:35:29 +0100

    Add authority boundary policy model

--- netengine/spec/__init__.py
+++ netengine/spec/__init__.py
@@ -1,10 +1,17 @@
 """Spec parsing and models for NetEngine declarative specifications."""
 
-from netengine.spec.authority import Authority, AuthorityKind, AuthorityScope, AuthoritySource
+from netengine.spec.authority import (
+    Authority,
+    AuthorityKind,
+    AuthorityScope,
+    AuthoritySource,
+    BoundaryPolicy,
+)
 
 __all__ = [
     "Authority",
     "AuthorityKind",
     "AuthorityScope",
     "AuthoritySource",
+    "BoundaryPolicy",
 ]
